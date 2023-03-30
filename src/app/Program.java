770cff0c461ef50669248b3fd04edbc3111a41e8
Leonardo Soares <95651574+Rox351@users.noreply.github.com>
2023-03-30 19:40:28 -0300

    30/03/2023

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,10 +1,16 @@
 package app;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		Pessoa p1 = new Pessoa();
+		
+		
+		System.out.println("Digite o seu nome: ");
+		p1.nome = input.nextLine();
 	}
 
 }
