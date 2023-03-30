ab2304094131908036c78c9de959865e27db586b
Leonardo Soares <95651574+Rox351@users.noreply.github.com>
2023-03-30 19:36:20 -0300

    30/03/2023

--- src/app/Pessoa.java
+++ src/app/Pessoa.java
@@ -1,5 +1,19 @@
 package app;
 
 public class Pessoa {
-
+	public String nome;
+	private int idade;
+	private double peso;
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
 }
