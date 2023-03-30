312bd52e79019a3b83b3f27547f9a40030859c6d
Leonardo Soares <95651574+Rox351@users.noreply.github.com>
2023-03-30 19:51:47 -0300

    30/03/2023

--- src/app/Program.java
+++ src/app/Program.java
@@ -11,6 +11,19 @@ public class Program {
 		
 		System.out.println("Digite o seu nome: ");
 		p1.nome = input.nextLine();
+		
+		System.out.println("Digite a sua idade: ");
+		p1.setIdade(input.nextInt());
+		
+		System.out.println("Digite o seu peso: ");
+		p1.setPeso(input.nextDouble());
+		
+		System.out.println("*****VISUALIZANDO OS DADOS*****");
+		System.out.println("Nome: " + p1.nome);
+		System.out.println("Idade: " + p1.getIdade());
+		System.out.println("Peso: " + p1.getPeso());
+		
+		input.close();
 	}
 
 }
